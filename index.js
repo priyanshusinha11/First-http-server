82aa79f22571795c5ff056c53f4bf8346b8a7e7b
piyanshu <piyanshusinha.2807@gmail.com>
2023-06-20 22:09:44 +0530

    filesh changed after the second class of week-2

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
 const port = 3000
-
+// function middleware1(req, res, next) {
+//     console.log("this is from inside middleware " + req.headers.counter);
+//     next();
+// }
+// app.use(middleware1);
+app.use(bodyParser.json())
 function calculateSum(counter) {
     var sum = 0;
     for (var i = 0; i < counter; i++) {
@@ -10,28 +15,35 @@ function calculateSum(counter) {
     }
     return sum;
 }
-
 function handleFirstRequest(req, res) {
     var counter = req.query.counter;
     var calculatedSum = calculateSum(counter);
-    console.log(calculatedSum);
-    var answer = "The answer is: " + calculatedSum;
-    res.send(answer)
+    var answerObject = {
+        sum: calculatedSum,
+    }
+    res.send(answerObject);
 }
 function createUser(req, res) {
     res.send("hello world");
 }
-
+function givePage(req, res) {
+    //     res.send(`<!DOCTYPE html>
+    // <html>
+    //   <head>
+    //     <title>Hi from page</title>
+    //   </head>
+    //   <body>
+    //     <b>Hey there!</b>
+    //   </body>
+    // </html>
+    // `)
+    res.sendFile(__dirname + "/index.html");
+}
+app.get('/', givePage)
 app.get('/handlesum', handleFirstRequest)
-app.post('/createUser', createUser)
-
 function started() {
     console.log(`Example app listening on port ${port}`)
 }
-
 app.listen(port, started)
-
-
-
 var calculatedSum = calculateSum(100);
 console.log(calculatedSum);
---
+++ secondProcess.js
@@ -0,0 +1,13 @@
+function logResponseBody(jsonBody) {
+    console.log(jsonBody);
+}
+
+
+function callbackFn(result) {
+    result.json().then(logResponseBody);
+}
+var sendObj = {
+    method: "GET"
+};
+
+fetch("http://localhost:3000/handlesum?counter=100", sendObj).then(callbackFn)
